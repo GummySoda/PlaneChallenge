d77b24dcc975d8370335ea203303f22386d1041d
Gummy Soda <millsa1@roverkids.org>
2024-11-20 13:54:43 -0500

    Finished Plane Challenge

---
+++ PlaneChallenge/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public GameObject Player;
+    private float Speed = 500
+
+       ;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Speed * Time.deltaTime);
+    }
+}
